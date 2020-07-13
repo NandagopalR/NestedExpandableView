fc2812b231565f5d2972e92da4bbbe2c7cffa3b1
Nandagopal R <gopal300491@gmail.com>
2020-07-13 21:18:35 +0530

    Some changes done

--- app/src/main/java/com/nanda/nestedexpandablerecyclerview/activity/MainActivity.java
+++ app/src/main/java/com/nanda/nestedexpandablerecyclerview/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.nanda.nestedexpandablerecyclerview.activity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ExpandableListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +12,7 @@ import com.nanda.nestedexpandablerecyclerview.data.model.CompanyModel;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ExpandableListView.OnGroupClickListener {
+public class MainActivity extends AppCompatActivity implements ExpandableListViewAdapter.GroupClickListener {
 
     private ExpandableListView expandableListView;
 
@@ -22,18 +21,23 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExpandableListViewAdapter adapter = new ExpandableListViewAdapter(this);
+        ExpandableListViewAdapter adapter = new ExpandableListViewAdapter(this, this);
         List<CompanyModel> companyModelList = CompanyMapper.getCompanyModelList();
 
         expandableListView = findViewById(R.id.expandableListView);
         expandableListView.setAdapter(adapter);
         adapter.setCompanyModelList(companyModelList);
-        expandableListView.setOnGroupClickListener(this);
     }
 
-
     @Override
-    public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-        return expandableListView.isGroupExpanded(groupPosition) ? expandableListView.collapseGroup(groupPosition) : expandableListView.expandGroup(groupPosition);
+    public void onGroupClicked(int selectedPosition, int groupPosition) {
+        if (selectedPosition == groupPosition) {
+            expandableListView.collapseGroup(selectedPosition);
+        } else if (selectedPosition != -1 && expandableListView.isGroupExpanded(selectedPosition)) {
+            expandableListView.collapseGroup(selectedPosition);
+            expandableListView.expandGroup(groupPosition);
+        } else {
+            expandableListView.expandGroup(groupPosition);
+        }
     }
 }
--- app/src/main/java/com/nanda/nestedexpandablerecyclerview/adapter/ExpandableListViewAdapter.java
+++ app/src/main/java/com/nanda/nestedexpandablerecyclerview/adapter/ExpandableListViewAdapter.java
@@ -24,10 +24,13 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
     private Context context;
     private List<CompanyModel> companyModelList;
+    private GroupClickListener groupClickListener;
+    private int selectedPosition = -1;
 
-    public ExpandableListViewAdapter(Context context) {
+    public ExpandableListViewAdapter(Context context, GroupClickListener groupClickListener) {
         this.context = context;
         companyModelList = new ArrayList<>();
+        this.groupClickListener = groupClickListener;
     }
 
     public void setCompanyModelList(List<CompanyModel> itemList) {
@@ -37,6 +40,101 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
         companyModelList.clear();
         companyModelList.addAll(itemList);
         notifyDataSetChanged();
+    }
+
+    public interface GroupClickListener {
+        void onGroupClicked(int selectedPosition, int groupPosition);
+    }
+
+    @Override
+    public View getGroupView(final int groupPosition, final boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_company, parent, false);
+        }
+
+        convertView.setTag(groupPosition);
+        CompanyModel companyModel = (CompanyModel) getGroup(groupPosition);
+        FrameLayout layoutGroupHeader = convertView.findViewById(R.id.layout_group);
+        TextView tvName = convertView.findViewById(R.id.tv_name);
+        ImageView imgArrow = convertView.findViewById(R.id.img_arrow);
+        imgArrow.setTag(groupPosition);
+
+        tvName.setText(companyModel.getCompanyName());
+        imgArrow.setImageResource(isExpanded ? R.drawable.ic_arrow_up : R.drawable.ic_arrow_down);
+        layoutGroupHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateList(groupPosition);
+                if (groupClickListener != null) {
+                    groupClickListener.onGroupClicked(selectedPosition, groupPosition);
+                }
+                selectedPosition = groupPosition;
+            }
+        });
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_desig, parent, false);
+        }
+
+        DesignationModel designationModel = (DesignationModel) getChild(groupPosition, childPosition);
+        EmployeeAdapter employeeAdapter = new EmployeeAdapter();
+
+        final ExpandableLayout expandableLayout = convertView.findViewById(R.id.expandable_layout);
+        FrameLayout layoutHeader = convertView.findViewById(R.id.layout_first_child);
+        TextView tvName = convertView.findViewById(R.id.tv_name);
+        ImageView imgArrow = convertView.findViewById(R.id.img_arrow);
+        RecyclerView recyclerView = convertView.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        String tag = groupPosition + "" + childPosition;
+        convertView.setTag(tag);
+        expandableLayout.setTag(tag);
+        layoutHeader.setTag(tag);
+        imgArrow.setTag(tag);
+        recyclerView.setTag(tag);
+        recyclerView.setAdapter(employeeAdapter);
+        tvName.setText(designationModel.getDesignation());
+
+        final boolean isExpanded = designationModel.isExpanded();
+        if (isExpanded) {
+            employeeAdapter.setEmployeeModelList(designationModel.getEmployeeModelList());
+        }
+        imgArrow.setImageResource(isExpanded ? R.drawable.ic_arrow_up : R.drawable.ic_arrow_down);
+        expandableLayout.setExpanded(isExpanded, false);
+
+        layoutHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DesignationModel designationModel = (DesignationModel) getChild(groupPosition, childPosition);
+                if (designationModel.isExpanded()) {
+                    designationModel.setExpanded(false);
+                } else {
+                    List<DesignationModel> designationModelList = companyModelList.get(groupPosition).getDesignationModelList();
+                    for (int i = 0, size = designationModelList.size(); i < size; i++) {
+                        DesignationModel model = designationModelList.get(i);
+                        if (i == childPosition) {
+                            model.setExpanded(true);
+                        } else {
+                            model.setExpanded(false);
+                        }
+                    }
+                }
+                notifyDataSetChanged();
+            }
+        });
+        return convertView;
+    }
+
+    private void updateList(int groupPosition) {
+        List<DesignationModel> designationModelList = companyModelList.get(groupPosition).getDesignationModelList();
+        for (int i = 0, size = designationModelList.size(); i < size; i++) {
+            DesignationModel model = designationModelList.get(i);
+            model.setExpanded(false);
+        }
     }
 
     @Override
@@ -79,75 +177,4 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
         return true;
     }
 
-    @Override
-    public View getGroupView(int groupPosition, final boolean isExpanded, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.item_company, parent, false);
-        }
-
-        convertView.setTag(groupPosition);
-        CompanyModel companyModel = (CompanyModel) getGroup(groupPosition);
-        TextView tvName = convertView.findViewById(R.id.tv_name);
-        ImageView imgArrow = convertView.findViewById(R.id.img_arrow);
-        imgArrow.setTag(groupPosition);
-
-        tvName.setText(companyModel.getCompanyName());
-        imgArrow.setImageResource(isExpanded ? R.drawable.ic_arrow_up : R.drawable.ic_arrow_down);
-        return convertView;
-    }
-
-    @Override
-    public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.item_desig, parent, false);
-        }
-
-        DesignationModel designationModel = (DesignationModel) getChild(groupPosition, childPosition);
-        EmployeeAdapter employeeAdapter = new EmployeeAdapter();
-
-        final ExpandableLayout expandableLayout = convertView.findViewById(R.id.expandable_layout);
-        FrameLayout layoutHeader = convertView.findViewById(R.id.layout_first_child);
-        TextView tvName = convertView.findViewById(R.id.tv_name);
-        ImageView imgArrow = convertView.findViewById(R.id.img_arrow);
-        RecyclerView recyclerView = convertView.findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-
-        String tag = groupPosition + "" + childPosition;
-        convertView.setTag(tag);
-        expandableLayout.setTag(tag);
-        layoutHeader.setTag(tag);
-        imgArrow.setTag(tag);
-        recyclerView.setTag(tag);
-        recyclerView.setAdapter(employeeAdapter);
-        tvName.setText(designationModel.getDesignation());
-
-        final boolean isExpanded = designationModel.isExpanded();
-        if (isExpanded) {
-            employeeAdapter.setEmployeeModelList(designationModel.getEmployeeModelList());
-        }
-        imgArrow.setImageResource(isExpanded ? R.drawable.ic_arrow_up : R.drawable.ic_arrow_down);
-        expandableLayout.setExpanded(isExpanded, true);
-
-        layoutHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DesignationModel designationModel = (DesignationModel) getChild(groupPosition, childPosition);
-                if (designationModel.isExpanded()) {
-                    designationModel.setExpanded(false);
-                } else {
-                    List<DesignationModel> designationModelList = companyModelList.get(groupPosition).getDesignationModelList();
-                    for (int i = 0, size = designationModelList.size(); i < size; i++) {
-                        DesignationModel model = designationModelList.get(i);
-                        if (i == childPosition) {
-                            model.setExpanded(true);
-                        } else {
-                            model.setExpanded(false);
-                        }
-                    }
-                }
-                notifyDataSetChanged();
-            }
-        });
-        return convertView;
-    }
 }
